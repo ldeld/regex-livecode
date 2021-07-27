250da3c44c7d57a35e4505b8ff4eba8f3ab88041
Lorenzo Del Castillo Detoeuf <l.delcastillo.97@gmail.com>
2021-07-27 18:26:21 +0200

    livecode done

--- french_ssn.rb
+++ french_ssn.rb
@@ -1,34 +1,31 @@
 require 'date'
 require 'YAML'
-
-
-SSN_REGEX = /^(?<gender>[12]) (?<year>\d{2}) (?<month>0[1-9]|1[0-2]) (?<place>0[1-9]|[1-9]\d) \d{3} \d{3} (?<key>\d{2})/
+SSN_REGEX = /^(?<gender>[12])\s(?<year>\d{2})\s(?<month>0[1-9]|1[012])\s(?<department>0[1-9]|[1-9]\d)(\s\d{3}){2}\s\d{2}$/
+DEPARTMENTS = YAML.load_file('data/french_departments.yml')
 
 def french_ssn_info(ssn_string)
+  # TODO
+  #match the regex to check if it matches correctly
   match_data = ssn_string.match(SSN_REGEX)
-  if match_data
-    # create the string from the match data
-    gender = get_gender(match_data[:gender])
-    month = get_month(match_data[:month].to_i)
-    year = "19#{match_data[:year]}"
-    place = get_place(match_data[:place])
-    "a #{gender}, born in #{month}, #{year} in #{place}."
+  if match_data && valid_key?(ssn_string)
+    gender = match_data[:gender] == '1' ? 'man' : 'woman'
+    month = Date::MONTHNAMES[match_data[:month].to_i]
+    year = "19" + match_data[:year]
+    department = DEPARTMENTS[code]
+
+    "a #{gender}, born in #{month}, #{year} in #{department}."
+    #to do
+    # if it does, do the matchdata
   else
     return "The number is invalid"
   end
 end
 
+def valid_key?(ssn_string)
+  key = ssn_string[-2..-1].gsub(' ', '').to_i
+  puts key
+  ssn_without_key = ssn_string[0..-3].gsub(' ', '').to_i
+  puts ssn_without_key
 
-def get_month(code)
-  Date::MONTHNAMES[code]
+  (97 - ssn_without_key) % 97 == key
 end
-
-def get_place(code)
-  departments = YAML.load_file('data/french_departments.yml')
-  departments[code]
-end
-
-def get_gender(code)
-  code == '1' ? 'man' : 'woman'
-end
-
--- spec/french_ssn_spec.rb
+++ spec/french_ssn_spec.rb
@@ -2,14 +2,23 @@ require_relative '../french_ssn.rb'
 
 describe '#french_ssn_info' do
   it 'should return "The number is invalid" when given an empty string' do
-    actual = french_ssn_info('')
-    expected = "The number is invalid"
+    expected = 'The number is invalid'
+    actual = french_ssn_info("")
+
     expect(actual).to eq(expected)
   end
 
-  it 'should correctly extract information from a valid security number' do
-    actual = french_ssn_info("1 84 12 76 451 089 46")
+  it 'should return "a man, born in December, 1984 in Seine-Maritime." when given "1 84 12 76 451 089 46"' do
     expected = "a man, born in December, 1984 in Seine-Maritime."
+    actual = french_ssn_info("1 84 12 76 451 089 46")
+
+    expect(actual).to eq(expected)
+  end
+
+   it 'should return "This numer is invalid" when the key is wrong' do
+    expected = "The number is invalid"
+    actual = french_ssn_info("1 84 12 76 451 089 48")
+
     expect(actual).to eq(expected)
   end
 end
